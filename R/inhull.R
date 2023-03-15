ae4fc8c92a1abe20be80ef885a3f061bb77550c7
Robert Lambert <120472879+rj-lambert@users.noreply.github.com>
2023-03-15 12:57:45 +0000

    Add files via upload

--- R/inhull.R
+++ R/inhull.R
@@ -1,4 +1,4 @@
-in_hull = function(point, input){
+in_hull2 = function(point, input){
   # function to determine if a point is in the convex hull
   
   # creating a plot to show where the point is in relation to the convex hull
@@ -15,10 +15,12 @@ in_hull = function(point, input){
     if (point[1] == jm_output[[j]][[1]][1]){
       if (point[2] == jm_output[[j]][[1]][2]){
         print(paste0("(", point[1], ",", point[2], ") is in the convex hull"))
+        break
       }
     }
     else{
       print(paste0("(", point[1], ",", point[2], ") is not in the convex hull"))
+      break
     }
   }
 }
